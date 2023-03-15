1540a83ea47578e1c1371ea5c3282732389819d9
lucasdefino <ldefino@mail.utdt.edu>
2023-03-15 11:33:23 -0300

    miercoles 15/03

--- misionhs_vs_evamisionhs/script.js
+++ misionhs_vs_evamisionhs/script.js
@@ -1,39 +1,51 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
-  // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({
 
-    //defino los ejes, elegi la marca dot para
-    //representar cada entidad
+    height: 500,
+    marginLeft: 70,
+    marginRight: 20,
+    marginBottom: 40,
+    marginTop: 40,
+    
     marks: [
-      Plot.dot(data, {
+      Plot.dot(data.filter(d => d.nacionalidad == 'EE.UU.' || d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad == 'Japon' || d.nacionalidad == 'China' || d.nacionalidad == 'Italia'), 
+        {
         y: 'mision_hs',
-        //agrego data al eje y
-        x: 'eva_mision_hs',
-        //resalto la marca
-        //r: 'nacionalidad',
-        //color del borde de la marca
-        //stroke: 'nacionalidad'
-        //fill agrupa segun 
+        x: 'eva_mision_hs', 
+        r: 'mision_hs',
         fill: 'nacionalidad',
-        //opacity: *entre 0 y 1*,
       }),
-    ],
 
-    //agrego el grid
+    ],
+    
+    
+    /*
+    Plot.legend({
+      filter : data.filter(d => d.nacionalidad == 'EE.UU.' || d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad == 'Japon' || d.nacionalidad == 'China' || d.nacionalidad == 'Italia'),
+      color: {
+        type: "categorical",
+        domain: d3.range(5).map(d => filter), 
+        range: ["green", "purple", "orange", "yellow", "blue",]
+      },
+    }), */
+
+    style:{
+      backgroundColor: 'black',
+      color: 'white',
+      fontFamily: 'monospace',
+    },
     x: { grid: true, line: true, zero: true, nice: true, },
     y: { grid: true, line: true, zero: true, nice: true, },
 
-    //parametros de la marca
-    //r: { range: [0, 50] },
-
-    //leyendas de color
+    r: { range: [0, 8] },
+    
     color: {
+      scheme: 'set1',
       legend: true,
     }
-    
+
 
   })
-  // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart)
 })
--- misionhs_vs_ocupacion_dif_genero/script.js
+++ misionhs_vs_ocupacion_dif_genero/script.js
@@ -1,13 +1,13 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
 
-  let chart = Plot.plot({
-
+  let chart = 
+  Plot.plot({
+    width: 800,
     facet: {
       data: data,
       x: 'genero',
     },
-
     marks: [
       Plot.barY(data, {
         x: 'ocupacion',
@@ -15,24 +15,22 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
         z: 'genero',
         stroke: 'genero',
         fill: 'genero',
-        sort: 'genero',
+        sort: 'genero',      
       }),
-      
+      Plot.axisX({label: null, lineWidth: 5, marginBottom: 40, anchor: 'bottom',}),
+      /*
+      Plot.legend({
+        color: {
+          type: "categorical",
+          domain: data.genero, 
+          range: ["green", "purple"]
+        }
+      })
+      */
     ],
-    x: {
-      tickFormat: 'd',
-    },
-
-    x: { grid: true, line: true, zero: true, nice: true, },
-    y: { grid: true, line: true, zero: true, nice: true, },
-
-  
-    color: {
-      legend: true,
-    }
-    
-
+    x: {grid: true, line: true, zero: true, nice: true, }, 
+    y: {grid: true, line: true, zero: true, nice: true, },
+    color: {legend: true, scheme: 'dark2'}    
   })
-  
   d3.select('#chart').append(() => chart)
 })
